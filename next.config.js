845c339d6f32bb3add0b59b17c1bfa2779a99c7c
paulogdm <paulogdemitri@gmail.com>
2019-01-17 12:01:20 -0200

    Now V2 fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withCss = require('@zeit/next-css');
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer');
 
 module.exports = withBundleAnalyzer(withCss({
+  target: "serverless",
   analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
   analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
   bundleAnalyzerConfig: {
